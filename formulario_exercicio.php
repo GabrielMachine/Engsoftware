a673d90c667e3cd5b89f17aa90f89f64ea0fec06
unknown <gabrielandradedesign@gmail.com>
2019-06-05 21:34:40 -0300

    mudanças de arquivos de formulário e atualizando o do modelo de negocio

--- formulario_exercicio.php
+++ formulario_exercicio.php
@@ -4,6 +4,6 @@
     <form action="trabsoftware.php" method="POST">
         Nome do exercicio: <input type="text" name="nomeE"><br>
         codigo do exercicio: <input type="text" name="codE"><br>
-        <input type="submit" value="Enviar Cadastro">
+        <input type="submit" value="Cadastrar exercício">
     </form>
 </body>
--- trabsoftware.php
+++ trabsoftware.php
@@ -1,13 +1,24 @@
+<html>
+<body>
+
 <?php
-
-class Aluno{
-
-    private $nome;
-    private $cpf;
-
-    public cadastrar_aluno(){
-
-        
+    class Aluno{
+        private $nome_aluno;
+        private $cpf_aluno;
+        public function cadastra_aluno(){
+            if($_POST["escolha"]=="aluno"){
+                $nome_aluno=$_POST["nomeU"];
+                echo $nome_aluno;
+                $cpf_aluno=$_POST["cpfU"];
+            }
+        }
+        public function get(){
+            return $this->$nome_aluno;
+        }
     }
-}
+echo Aluno::get();
 ?>
+
+
+</body>
+</html>
