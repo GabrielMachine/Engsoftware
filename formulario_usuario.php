e53b9a8fd54b2c6aba7136fe0518c363f6d0415c
unknown <gabrielandradedesign@gmail.com>
2019-06-05 21:28:53 -0300

    formulario de usuário em php

---
+++ formulario_usuario.php
@@ -0,0 +1,15 @@
+<html>
+        <body>
+        
+        <form action="trabsoftware.php" method="POST">
+        Nome:<br>
+        <input type="text" name="nomeU"><br>
+        CPF:<br>
+        <input type="text" name="cpfU"><br>
+        <input type="radio" name="escolha" value="instrutor">instrutor
+        <input type="radio" name="escolha" value="aluno">aluno<br>
+        <input type="submit"  value="Cadastrar"><br>
+        </form>
+        
+        </body>
+</html> 
